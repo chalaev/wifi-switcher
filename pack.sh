50391717537246b65fc31c7a9d3585b4a88ab09f
Oleg Shalaev <chalaev@gmail.com>
2016-07-17 22:52:36 -0400

    fixed ftp server configuration

--- pack.sh
+++ pack.sh
@@ -1,4 +1,5 @@
-#!/bin/sh
+#!/bin/bash
+# requires packages: debhelper dh-make fakeroot build-essential config-package-dev
 if [ -e debian/control ]; then
     projectName=$(awk "\$1==\"Package:\" {print \$2}"  debian/control)
 fi
@@ -6,9 +7,9 @@ if [ -z "$projectName" ] || [ "$projectName" != "wifi-switcher" ]; then
     echo "This is a wrong directory, please change to correct one"
     exit 1
 fi
-find /srv/local-apt-repository/ -name "wifi-switcher*.deb" -exec rm {} \;
-if [ -e debian/wifi-switcher ]; then
-    rm -r debian/wifi-switcher
+find /srv/local-apt-repository/ -name "${projectName}*.deb" -exec rm {} \;
+if [ -e debian/${projectName} ]; then
+    rm -r debian/${projectName}
 fi
 # remove dangling tabs left by emacs shell-script-mode:
 find debian/ -type f \( -name "pre*" -o -name "post*" \) -exec sed -i "s/[ \t]\+$//" {} \;
@@ -16,18 +17,36 @@ find debian/ -type f \( -name "pre*" -o -name "post*" \) -exec sed -i "s/[ \t]\+
 # generate man-files from emacs org-mode ones (note that the original ox-man.el is buggy):
 if `ls /usr/share/emacs/*/lisp/org/ox-man.elc > /dev/null 2>&1` ; then
     oxman=$(ls /usr/share/emacs/*/lisp/org/ox-man.elc)
-    oxman=$(echo "$oxman" | head -n1)
-    for i in wifi-switcher*.org; do
+oxman=$(echo "$oxman" | head -n1)
+    for i in ${projectName}*.org; do
 	wsn=`basename $i .org`
 	emacs  ${wsn}.org --batch -l $oxman -f org-man-export-to-man --kill
 	sed -i  -e "s/\\\s[+-]2//g" -e "s/\\\u//g" ${wsn}.man
 	mv ${wsn}.man man/
     done
 fi
-rm ../wifi-switcher_*
-dh_make -c gpl -e chalaev@gmail.com --indep --yes --createorig
-dpkg-buildpackage -rfakeroot -us -uc
+rm ../${projectName}_*
+
+# задача: прочитать версию из файла или из опции:
+#version="1.0"
+if [ -e current_version.txt ] ; then read version < current_version.txt ; else version="1.0" ; fi
+version=$(echo  $version + 0.1 | bc)
+while getopts v: opt ; do
+    case "$opt" in
+	v)  version="$OPTARG" ;;
+	\?) echo "Invalid option: -$OPTARG" >&2  ;;
+    esac
+done
+LC_NUMERIC=C version=$(printf "%1.1f" $version)
+# temporary fix because for version number ≠1.0 dpkg-buildpackage below fails:
+version="1.0"
+echo "Packaging the ${version}th version."
+dh_make -p ${projectName}_${version} -c gpl -e chalaev@gmail.com --indep --yes --createorig
+echo $version > current_version.txt
+
+# следующая строчка не работает, конфликт версий (если версия не равна 1.0)
+dpkg-buildpackage -rfakeroot -us -uc -v$version
 # See the package local-apt-repository:
 if [ -d /srv/local-apt-repository/ ]; then
-    mv  ../wifi-switcher*.deb /srv/local-apt-repository/
+    mv  ../${projectName}*.deb /srv/local-apt-repository/
 fi
--- usr/share/wifi-switcher/hotspot.sh
+++ usr/share/wifi-switcher/hotspot.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
-
+# starts and stops hotspot for the wifi-switcher package
 wifi_interface="wlan0"
+essid=`hostname`
 [ -r /etc/default/wifi-switcher ] && . /etc/default/wifi-switcher
 
 case "$1" in
 start)
     sudo /usr/sbin/service wifi-switcher stop
     # not needed: sudo /sbin/ifdown ${wifi_interface}
-    sudo /sbin/ifup ${wifi_interface}=essid
+    sudo /sbin/ifup ${wifi_interface}=wifiswitcher
     for i in hostapd isc-dhcp-server vsftpd; do
 	echo "starting $i"
 	sudo /usr/sbin/service $i start
@@ -30,7 +31,7 @@ info)
     if [ -r /etc/hostapd/wifi-switcher.conf ] ; then
 	awk -F= '$1 == "ssid" {print "The SSID of the hotspot wifi network is "$2}' /etc/hostapd/wifi-switcher.conf
 	awk -F= '$1 == "wpa_passphrase" {print "The wpa2-passsword for the hotspot wifi network is "$2}' /etc/hostapd/wifi-switcher.conf
-	"Run /usr/share/wifi-switcher/get-passwords as root to see the ftp password"
+	echo "Run /usr/share/wifi-switcher/get-passwords as root to see the ftp password"
     else
 	echo "can not read /etc/hostapd/wifi-switcher.conf, consider running me with root priviledges"
     fi
@@ -40,30 +41,12 @@ info)
     # TO BE TESTED in case when there are no wifi networks
     csu=$(sudo /sbin/iwlist ${wifi_interface} scan | grep Channel: | sed "s/[ \t]*Channel:\([0-9]\+\)/\1/"  | sort -n)
     echo "The following wifi channels are in use"
-    ucsu=$(echo "$csu" | uniq)
+    ucsu=$(echo "$csu" | uniq); echo $ucsu
     echo "these channels are used respectively by so many wifi networks:"
     for i in $ucsu ; do echo "$csu" | grep "^$i$" | wc -l ; done
     echo "If your devices experience problems connecting to your hotspot, consider setting a (the least used) channel number in /etc/network/interfaces.d/wifi-switcher"
     ;;
-bugreport)
-    echo "This is a bugreport for wifi-switcher"
-    for com in "ip addr ls" "ls -lth /etc/default/hostapd*" "ls -lth /etc/default/isc*" \
-"ls -lth /var/lib/dhcp/dhc*leases" "ls /{var/,/}run/{udhcpc,wpa_*,hostapd,vsftp}.pid"  "ls -lthR /etc/wpa_supplicant*" \
-"ls -lthR /etc/vsftp*" "ls -lthR /etc/hostapd*" "ls -lthR /etc/dhcp*" "tail -n30 /var/log/wifi-switcher.log" ; do
-	echo "####\nThe output of the \"$com\" command is"
-	LANG=C $com
-    done
-    # We will hide some fields in the files:
-    maskFields="identity password psk wpa_passphrase"
-    hidePass=""; for fi in $maskFields ; do hidePass="$hidePass -e s/${fi}=.*/${fi}=xxx/" ; done
-    for cf in /etc/default/hostapd /etc/default/isc-dhcp-server /etc/pam.d/vsftpd /etc/vsftpd.conf \
-/etc/dhcp/wifi-switcher.conf /etc/wpa_supplicant/wifi-switcher.conf \
-/etc/hostapd/wifi-switcher.conf /etc/network/interfaces ; do
-	echo "####\nThe contents of the \"$cf\" is"
-	sed $hidePass $cf | grep -v ^#
-    done
-    ;;
 *)
-    echo $"Usage: $0 {start|stop|info|bugreport}"
+    echo $"Usage: $0 {start|stop|info}"
     exit 1
 esac
