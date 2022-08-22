3fb591cfe3d6a9b08dd061576f1ad1f0d1c5269e
Oleg Shalaev <oleg@chalaev.com>
2022-08-22 17:39:29 -0400

    uploaded deb package

--- pack.sh
+++ pack.sh
@@ -16,8 +16,6 @@ if [ -z "$projectName" ] || [ "$projectName" != "wifi-switcher" ]; then
     echo "This is a wrong directory, please change to correct one"
     exit 1
 fi
-# commented it out 2020-05-01:
-# find /srv/local-apt-repository/ -name "${projectName}*.deb" -exec rm {} \;
 
 if [ -e debian/${projectName} ]; then
     rm -r debian/${projectName}
@@ -26,16 +24,12 @@ fi
 find debian/ -type f \( -name "pre*" -o -name "post*" \) -exec sed -i "s/[ \t]\+$//" {} \;
 
 # generate man-files from emacs org-mode ones (note that the original ox-man.el is buggy):
-#if `ls /usr/share/emacs/*/lisp/org/ox-man.elc > /dev/null 2>&1` ; then
 if [ -e /usr/share/emacs/site-lisp/org-mode/ox-man.el ]  ; then
-    # oxman=$(ls /usr/share/emacs/*/lisp/org/ox-man.elc)
-    # oxman=$(echo "$oxman" | head -n1)
     oxman="/usr/share/emacs/site-lisp/org-mode/ox-man.el"
     for i in ${projectName}*.org; do
-	wsn=`basename $i .org`
-	emacs  ${wsn}.org --batch -l $oxman -f org-man-export-to-man --kill
+	wsn=$(basename $i .org)
+	emacs --no-site-file --batch ${wsn}.org -l $oxman -f org-man-export-to-man
 	# the following sed fixes consequences of ox-man.el bugs:
-#	sed  -e "s/\\\s[+-]2//g" -e "s/\\\u//g" -e "s/\.br$/\n.br/" -e "s/\$\\\\/\\\\\\\\/" -i ${wsn}.man
 	sed  -e "s/\\\s[+-]2//g" -e "s/\\\u//g" -e "s/\.br$/\n.br/" -e  's/^\.TH "" "1"/.TH "wifi-switcher" "1"/' -i ${wsn}.man
 	mv ${wsn}.man man/
     done
@@ -45,14 +39,9 @@ rm ../${projectName}_*
 # https://www.debian.org/doc/debian-policy/ch-source.html#s-dpkgchangelog
 # version="1.0"
 # if [ -e current_version.txt ] ; then read version < current_version.txt ; else version="1.0" ; fi
-version=`dpkg-parsechangelog --show-field Version | sed "s/-[0-9]\+$//"`
+version=$(dpkg-parsechangelog --show-field Version | sed "s/-[0-9]\+$//")
 echo "Packaging the ${version}th version."
-# mv -i .git ../wifi-switcher.git
-# mv -i .pc ../wifi-switcher.pc
 dh_make -p ${projectName}_${version} -c gpl -e oleg@chalaev.com --indep --yes --createorig
-# mv -i  ../wifi-switcher.pc .pc
-# mv -i  ../wifi-switcher.git .git
-# echo $version > current_version.txt
 
 # dpkg-buildpackage -rfakeroot -us -uc -v$version
 # dpkg-buildpackage -rfakeroot -uc -v$version
